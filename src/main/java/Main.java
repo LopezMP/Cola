ce8bd62aed771a6f70b18cebee90438a98cf8eb0
LopezMP <LopezMP@users.noreply.github.com>
2019-03-21 02:08:25 +0100

    falta

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue<Integer> q1 = new QueueImpl<Integer>(6);
+        Queue<String> q2 = new QueueImpl<String>(8);
+       // Queue<User> q3 = new QueueImpl<User>(11);
+
+    }
+}
